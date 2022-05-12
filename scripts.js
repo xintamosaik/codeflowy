0ca37a0ca65eb650a325becc3cc1f9b4b511db1a
xintamosaik <xintamosaik@gmail.com>
2022-05-12 20:31:29 +0200

    Update scripts.js
    add init and test functions

--- scripts.js
+++ scripts.js
@@ -1,1 +1,13 @@
+const test = () => {
+  const paragraph = document.createElement(p)
+  paragraph.innerHTML = 'Scripts from scripts.js haven been loaded succesfully'
+  document.appendChild(paragraph)
+  
+}
 
+const init = () => {
+  
+
+}
+init()
+test()
